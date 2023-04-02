ea8db1a43acbb8379105391b3506a0fe4fc0072e
Muhid Hossain <muhidhossain7@gmail.com>
2023-04-02 15:59:32 +0600

    learned streams and buffer

--- index.js
+++ index.js
@@ -1,30 +1,7 @@
-const PizzaShop = require('./pizza-shop');
-const DrinkMachine = require('./drink-machine');
+const buffer = new Buffer.from('Muhid');
 
-const pizzaShop = new PizzaShop();
-const drinkMachine = new DrinkMachine();
+buffer.write('Codevolution');
 
-pizzaShop.on('order', (size, topping) => {
-  console.log(`Order received! Baking a ${size} pizza with ${topping}`);
-  drinkMachine.serveDrink(size);
-});
-
-pizzaShop.order('large', 'mushrooms');
-pizzaShop.displayOrderNumber();
-
-// const EventEmitter = require('node:events');
-
-// const emitter = new EventEmitter();
-
-// emitter.on('order-pizza', (size, topping) => {
-//   console.log(`Order received! Baking a ${size} pizza with ${topping}`);
-// });
-
-// emitter.on('order-pizza', (size) => {
-//   if (size === 'large') {
-//     console.log('Serving complimentary drink');
-//   }
-// });
-
-// console.log('Do work before event occurs in the system');
-// emitter.emit('order-pizza', 'large', 'mushrooms');
+console.log(buffer.toString());
+console.log(buffer.toJSON());
+console.log(buffer);
