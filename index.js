5c9f1bee3e839a0aecdbe807af3f5845e2ed2170
Muhid Hossain <muhidhossain7@gmail.com>
2023-04-07 16:18:02 +0600

    Streams

--- index.js
+++ index.js
@@ -1,42 +1,13 @@
-const fs = require('node:fs/promises');
+const fs = require('node:fs');
 
-console.log('First');
+const readableStream = fs.createReadStream('./file.txt', {
+  encoding: 'utf-8',
+  highWaterMark: 2,
+});
 
-async function readFile() {
-  try {
-    const data = await fs.readFile('file.txt', 'utf-8');
-    console.log(data);
-  } catch (err) {
-    console.log(err);
-  }
-}
+const writeableStream = fs.createWriteStream('./file2.txt');
 
-readFile();
-
-// const fs = require('node:fs');
-
-// console.log('First');
-// const fileContents = fs.readFileSync('./file.txt', 'utf-8');
-// console.log(fileContents);
-
-// console.log('Second');
-
-// fs.readFile('./file.txt', 'utf-8', (error, data) => {
-//   if (error) {
-//     console.log(error);
-//   } else {
-//     console.log(data);
-//   }
-// });
-
-// console.log('Third');
-
-// fs.writeFileSync('./greet.txt', 'Hello world!');
-
-// fs.writeFile('./greet.txt', ' Hello Muhid!', { flag: 'a' }, (err) => {
-//   if (err) {
-//     console.log(err);
-//   } else {
-//     console.log('File written');
-//   }
-// });
+readableStream.on('data', (chunk) => {
+  console.log(chunk);
+  writeableStream.write(chunk);
+});
