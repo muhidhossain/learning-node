454cca704a641502e042d18e6e121bdfb61578d7
Muhid Hossain <muhidhossain7@gmail.com>
2023-04-07 16:07:31 +0600

    fs module and Promise module

--- index.js
+++ index.js
@@ -1,7 +1,42 @@
-const buffer = new Buffer.from('Muhid');
+const fs = require('node:fs/promises');
 
-buffer.write('Codevolution');
+console.log('First');
 
-console.log(buffer.toString());
-console.log(buffer.toJSON());
-console.log(buffer);
+async function readFile() {
+  try {
+    const data = await fs.readFile('file.txt', 'utf-8');
+    console.log(data);
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+readFile();
+
+// const fs = require('node:fs');
+
+// console.log('First');
+// const fileContents = fs.readFileSync('./file.txt', 'utf-8');
+// console.log(fileContents);
+
+// console.log('Second');
+
+// fs.readFile('./file.txt', 'utf-8', (error, data) => {
+//   if (error) {
+//     console.log(error);
+//   } else {
+//     console.log(data);
+//   }
+// });
+
+// console.log('Third');
+
+// fs.writeFileSync('./greet.txt', 'Hello world!');
+
+// fs.writeFile('./greet.txt', ' Hello Muhid!', { flag: 'a' }, (err) => {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log('File written');
+//   }
+// });
