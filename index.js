c74d410faf4f7777a2df68489f1465978892516e
Muhid Hossain <muhidhossain7@gmail.com>
2023-04-10 11:36:02 +0600

    HTML response

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
 const http = require('node:http');
+const fs = require('node:fs');
 
 const server = http.createServer((req, res) => {
-  const superHero = {
-    firstName: 'Bruce',
-    lastName: 'Wayne',
-  };
-
-  res.writeHead(200, { 'Content-Type': 'application/json' });
-  res.end(JSON.stringify(superHero));
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+  fs.createReadStream(__dirname + '/index.html').pipe(res);
+  // const html = fs.readFileSync('./index.html', 'utf-8');
+  // res.end(html);
 });
 
 server.listen(3000, () => {
