211bed0c092191e59277c70c54fd18ad17a9ee21
Muhid Hossain <muhidhossain7@gmail.com>
2023-04-11 11:47:08 +0600

    HTTP Routing

--- index.js
+++ index.js
@@ -2,11 +2,24 @@ const http = require('node:http');
 const fs = require('node:fs');
 
 const server = http.createServer((req, res) => {
-  const name = 'Muhid';
-  res.writeHead(200, { 'Content-Type': 'text/html' });
-  let html = fs.readFileSync('./index.html', 'utf-8');
-  html = html.replace('{{name}}', name);
-  res.end(html);
+  if (req.url === '/') {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Home page');
+  } else if (req.url === '/about') {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('About page');
+  } else if (req.url === '/api') {
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.end(
+      JSON.stringify({
+        firstName: 'Muhid',
+        lastName: 'Hossain',
+      })
+    );
+  } else {
+    res.writeHead(404);
+    res.end('Page not found');
+  }
 });
 
 server.listen(3000, () => {
