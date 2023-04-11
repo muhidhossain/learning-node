692ece64bd587693b7558a6b9df2066f577e3205
Muhid Hossain <muhidhossain7@gmail.com>
2023-04-11 10:51:34 +0600

    HTML Template

--- index.js
+++ index.js
@@ -2,10 +2,11 @@ const http = require('node:http');
 const fs = require('node:fs');
 
 const server = http.createServer((req, res) => {
+  const name = 'Muhid';
   res.writeHead(200, { 'Content-Type': 'text/html' });
-  fs.createReadStream(__dirname + '/index.html').pipe(res);
-  // const html = fs.readFileSync('./index.html', 'utf-8');
-  // res.end(html);
+  let html = fs.readFileSync('./index.html', 'utf-8');
+  html = html.replace('{{name}}', name);
+  res.end(html);
 });
 
 server.listen(3000, () => {
